87bfb6acc534a8f5be03bd957c487c1f36b79240
Ynm123 <3264354544@qq.com>
2026-08-16 22:08:50 +0800

    chore: 关闭背景视频并更换壁纸 API

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -4,7 +4,7 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 	// 壁纸模式："banner" 横幅壁纸，"fullscreen" 全屏壁纸，"overlay" 全屏透明，"none" 纯色背景无壁纸
 	mode: "fullscreen",
 	// 是否启用背景视频播放，配置后将在导航栏显示视频播放按钮
-	playerEnable: true,
+	playerEnable: false,
 	/**
 	 * 背景图片配置
 	 * 图片路径支持三种格式：
@@ -38,10 +38,16 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 	src: {
 		// 桌面背景图片（支持单张或多张随机）
 		// desktop: "assets/images/DesktopWallpaper/d1.avif",
-		desktop: "https://tu.ltyuanfang.cn/api/fengjing.php",
+		desktop: [
+			"https://wp.upx8.com/api.php?category=nature",
+			"https://wp.upx8.com/api.php?category=space",
+		],
 		// 移动背景图片（支持单张或多张随机）
 		// mobile: "assets/images/MobileWallpaper/m1.avif",
-		mobile: "https://tu.ltyuanfang.cn/api/fengjing.php",
+		mobile: [
+			"https://wp.upx8.com/api.php?category=nature",
+			"https://wp.upx8.com/api.php?category=space",
+		],
 		// 背景视频播放地址
 		// 支持单个视频路径（字符串）或多个视频循环（数组，参考上面壁纸配置）
 		// 支持远程视频URL，本地视频请放在 public/assets/videos/ 目录下
