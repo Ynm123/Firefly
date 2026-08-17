2fd5ad1255c67ad4af9f1b8333169ec7385ec625
Ynm123 <3264354544@qq.com>
2026-08-17 14:17:54 +0800

    feat: 添加 51.la 页脚访问统计

--- src/config/analyticsConfig.ts
+++ src/config/analyticsConfig.ts
@@ -34,11 +34,11 @@ export const analyticsConfig: AnalyticsConfig = {
 	// 51la 统计配置
 	la51Analytics: {
 		// 51la 统计 ID
-		Id: "",
+		Id: "3QuNK0eBBZS0LHtp",
 		// 自定义 SDK JS 地址，防止 DNS 污染，留空使用默认地址
 		sdkUrl: "",
 		// 多个统计 ID 的数据分离标识，留空则使用 Id
-		ck: "",
+		ck: "3QuNK0eBBZS0LHtp",
 		// 是否开启事件分析功能
 		autoTrack: false,
 		//  Hash路由模式, 项目使用History API路由, 所以不必开启默认false
